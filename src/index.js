401ba8c5a6cb5ec7857361b440193f341f0b2c66
ValysyaPoncho <valentin.ivancikov@gmail.com>
2018-02-25 22:55:20 +0300

    7th solution. v2.0

--- src/index.js
+++ src/index.js
@@ -2,28 +2,23 @@ module.exports =
 function getZerosCount(number) {
     var twoNumbers = 0, fiveNumbers = 0;
 
-    for (var i = 1; i <= number; i++) {
-      if (i % 2 === 0) {
-        var foo1 = i;
-
-        while(foo1 % 2 === 0) {
-          twoNumbers += 1;
-          foo1 /= 2;
-        }
-      }
-
-      if (i % 5 === 0) {
-        var foo2 = i;
-
-        while(foo2 % 5 === 0) {
-          fiveNumbers += 1;
-          foo2 /= 5;
-        }
+    for (var n = 1, i = iter = Math.pow(2,n); i <= number; n++, i = iter = Math.pow(2,n)) {
+      for (var j = i; j <= number; j += iter * 2) {
+        twoNumbers += n;
       }
     }
 
+    for (var n = 1, i = iter = Math.pow(5,n); i <= number; n++, i = iter = Math.pow(5,n)) {
+      for (var j = i; j <= number; j += iter) {
+        fiveNumbers += 1;
+      }
+    }
+    
     if (fiveNumbers >= twoNumbers)
       return twoNumbers;
     else
       return fiveNumbers;  
 }
+
+//var zerosCount = getZerosCount(84583674);
+//console.log(zerosCount);
